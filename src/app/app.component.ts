0aad4d52c7c2339fd90aaabc587038665a163498
Cindy Berghuizen <c.berghuizen@gmail.com>
2016-12-11 13:26:26 +0100

    working form on desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl ,FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  registerForm : FormGroup;
+  
+  constructor(private formBuilder : FormBuilder){}
+
+  ngOnInit(){
+    this.registerForm = this.formBuilder.group({
+      field1: ['', Validators.compose([Validators.pattern('^[a-zA-Z0-9.]{5,6}$'), Validators.required])],
+      field2: ['', Validators.compose([Validators.pattern('^[a-zA-Z.]{2,30}$'), Validators.required])]
+     
+    });
+  }
+
+  onSubmit() {
+     //get Data and go to next page
+       
+    }
+
 }
