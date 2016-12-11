48673114a2f66f2b0ad641147f2400f41c030bc8
Cindy Berghuizen <c.berghuizen@gmail.com>
2016-12-11 14:25:26 +0100

    minor tweaks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,12 +3,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import {
+  FormGroup,
+  ReactiveFormsModule
+} from '@angular/forms';
+
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
-      ],
+      ], imports: [ReactiveFormsModule]
     });
     TestBed.compileComponents();
   });
@@ -19,16 +24,15 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
+  it('should render two fields', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+
+    it('should create a `FormGroup` comprised of `FormControl`s', () => {
+      compiled.ngOnInit();
+      expect(compiled.formGroup instanceof FormGroup).toBe(true);
+    });
   }));
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl ,FormBuilder, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+ 
 })
 export class AppComponent implements OnInit {
 
@@ -15,14 +17,14 @@ export class AppComponent implements OnInit {
 
   ngOnInit(){
     this.registerForm = this.formBuilder.group({
-      field1: ['', Validators.compose([Validators.pattern('^[a-zA-Z0-9.]{5,6}$'), Validators.required])],
-      field2: ['', Validators.compose([Validators.pattern('^[a-zA-Z.]{2,30}$'), Validators.required])]
+      bookingcode: ['', Validators.compose([Validators.pattern('^[a-zA-Z0-9.]{5,6}$'), Validators.required])],
+      familyname: ['', Validators.compose([Validators.pattern('^[a-zA-Z.]{2,30}$'), Validators.required])]
      
     });
   }
 
   onSubmit() {
-     //get Data and go to next page
+     //if the bookingcode from bookingcode is in the bookingcode json, go to component.
        
     }
 
